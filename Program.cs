69f4cd83c00fac93e28f66088ca86b2e5c7e1111
Alex <pf775_ash@rambler.ru>
2022-06-02 20:50:42 +0300

    flow

--- Program.cs
+++ Program.cs
@@ -6,70 +6,59 @@ namespace Flow
 {
     class Program
     {
-        static bool exit = false;
         static double j = 1;
-        async static Task Method(object cancel, string textMethod)
+        static void Method(object cancel, string textMethod)
         {
             var token = (CancellationToken)cancel;
 
-            token.ThrowIfCancellationRequested();
-            if (token.IsCancellationRequested)
-            {
-                Console.WriteLine("Получен запрос на отмену задачи.");
-                token.ThrowIfCancellationRequested();
-            }
             Random rand = new Random();
-            int y = rand.Next(100, 2000);
+            int y = rand.Next(200, 1000);
             Console.WriteLine($"{textMethod} {Task.CurrentId}");
-            for (int i = 1; i < 15; i++)
+            for (int i = 1; i < 10; i++)
             {
                 j *= i;
-                await Task.Delay(y);
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Получен запрос на отмену задачи.");
+                    throw new OperationCanceledException();
+                }
+                //Task.Delay(y);
+                Thread.Sleep(y);
             }
-            Console.WriteLine(j);
-            j = 1;
+            Console.WriteLine(j);            
         }
-        async static Task Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("ESC - выход");
 
             var cancelTokSrc = new CancellationTokenSource();
-            await Task.Run(() =>
+            Task.Run(() =>
                 {
                     Method(cancelTokSrc.Token, "Первый пошел");
                 });
-            await Task.Run(() =>
+            Task.Run(() =>
             {
                 Method(cancelTokSrc.Token, "Второй пошел");
             });
-            await Task.Run(() =>
+            Task.Run(() =>
             {
                 Method(cancelTokSrc.Token, "Третий пошел");
             });
-            await Task.Run(() =>
+            Task.Run(() =>
             {
                 Method(cancelTokSrc.Token, "Четвертый пошел");
             });
-            await Task.Run(() =>
+            Task.Run(() =>
             {
                 Method(cancelTokSrc.Token, "Пятый пошел");
             });
-            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            while (true)
             {
-                try
+                if(Console.ReadKey().Key == ConsoleKey.Escape)
                 {
                     cancelTokSrc.Cancel();
-                }
-                catch (AggregateException e)
-                {
-                    Console.WriteLine("\nЗадача task отменена.\n");
-
-                    Console.WriteLine("- " + e.InnerException.Message);
-                }
-                finally
-                {
-                    cancelTokSrc.Dispose();
-                }
+                    break;
+                }                
             }  
         }
     }
